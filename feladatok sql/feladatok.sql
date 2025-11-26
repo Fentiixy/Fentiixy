14773eda8cf575bbdcb2b6633121601a2dc0a566
Kővári Tamás <156439476+Fentiixy@users.noreply.github.com>
2025-11-26 19:23:16 +0100

    fel

--- feladatok sql/feladatok.sql
+++ feladatok sql/feladatok.sql
@@ -35,7 +35,7 @@ WHERE emberek.Beosztas = 'orvos'
 ORDER BY emberek.Nev ASC;
 
 #8
-SELECT *
+SELECT emberek.Nev
 FROM emberek
 WHERE emberek.Beosztas = 'ápoló'
   AND emberek.Nev LIKE '%Balázs';
@@ -51,3 +51,14 @@ FROM Emberek
 WHERE Emberek.Nem = 'f'
 ORDER BY Emberek.Netto DESC;
 
+#11
+SELECT Emberek.Nev
+FROM Emberek
+WHERE Emberek.netto > 40000
+ORDER BY emberek.Nev ASC;
+
+#12
+SELECT Emberek.Nev
+FROM Emberek
+WHERE Emberek.Netto > 50000 AND Emberek.Netto < 110000
+ORDER BY Emberek.Nev ASC;
