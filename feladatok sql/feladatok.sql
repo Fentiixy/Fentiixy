ad62fa6e7b5280db5e2d077dd14d13b584f6f757
Kővári Tamás <156439476+Fentiixy@users.noreply.github.com>
2025-12-07 23:27:45 +0100

    felada

--- feladatok sql/feladatok.sql
+++ feladatok sql/feladatok.sql
@@ -1,64 +1,104 @@
-#1
-SELECT emberek.Nev, emberek.Netto
-FROM Emberek
-ORDER BY emberek.Nev;
-
-#2
-SELECT emberek.Nev
-FROM Emberek
-ORDER BY emberek.Netto DESC;
-
-#3
-SELECT emberek.Nev, emberek.Netto
-FROM Emberek
-ORDER BY emberek.GyerekSzam ASC;
-
-#4
-SELECT Emberek.Osztaly, emberek.Nev, emberek.Netto
-FROM Emberek
-ORDER BY emberek.Nev DESC, emberek.Netto ASC;
-
-#5
-SELECT Emberek.Nev, Emberek.GyerekSzam
-FROM Emberek
-ORDER BY Emberek.GyerekSzam DESC, Emberek.Nev ASC;
-
-#6
-SELECT Emberek.Nev, emberek.Beosztas, emberek.Osztaly, emberek.Netto
-FROM Emberek
-ORDER BY emberek.Beosztas DESC, emberek.Osztaly ASC, emberek.Netto ASC;
-
-#7
-SELECT emberek.Nev
+1. Feladat: 
+SELECT emberek.nev, emberek.netto
 FROM emberek
-WHERE emberek.Beosztas = 'orvos'
-ORDER BY emberek.Nev ASC;
+ORDER BY emberek.nev;
 
-#8
-SELECT emberek.Nev
+2. Feladat:
+SELECT emberek.nev, emberek.netto
 FROM emberek
-WHERE emberek.Beosztas = 'ápoló'
-  AND emberek.Nev LIKE '%Balázs';
+ORDER BY emberek.netto DESC;
 
-#9
-SELECT emberek.Nev, Emberek.GyerekSzam
+3. Feladat:
+SELECT emberek.nev, emberek.netto, emberek.gyerekszam
 FROM emberek
-WHERE emberek.Nev LIKE 'F% Rita';
+ORDER BY emberek.gyerekszam ASC;
 
-#10
-SELECT Emberek.Nev, Emberek.Netto
-FROM Emberek
-WHERE Emberek.Nem = 'f'
-ORDER BY Emberek.Netto DESC;
+4. Feladat:
+SELECT emberek.nev, emberek.netto, emberek.osztaly
+FROM emberek
+ORDER BY emberek.gyerekszam DESC, emberek.netto ASC;
 
-#11
-SELECT Emberek.Nev
-FROM Emberek
-WHERE Emberek.netto > 40000
-ORDER BY emberek.Nev ASC;
+5. Feladat:
+SELECT emberek.gyerekszam, emberek.nev
+FROM emberek
+ORDER BY emberek.gyerekszam DESC, emberek.nev;
 
-#12
-SELECT Emberek.Nev
-FROM Emberek
-WHERE Emberek.Netto > 50000 AND Emberek.Netto < 110000
-ORDER BY Emberek.Nev ASC;
+6.Feladat:
+SELECT emberek.nev, emberek.beosztas, emberek.osztaly, emberek.netto
+FROM emberek
+ORDER BY emberek.beosztas DESC, emberek.osztaly ASC, emberek.netto ASC;
+
+7.Feladat:
+SELECT emberek.nev, emberek.beosztas
+FROM emberek
+WHERE emberek.beosztas = "orvos"
+ORDER BY emberek.nev ASC
+
+8.Feladat:
+SELECT emberek.nev, emberek.beosztas
+FROM emberek
+WHERE emberek.beosztas = "ápoló" AND emberek.nev LIKE "%Balázs";
+
+9.Feladat:
+SELECT emberek.nev, emberek.gyerekszam
+FROM emberek
+WHERE emberek.nev LIKE "F%Rita"
+
+10.Feladat:
+SELECT emberek.nev, emberek.nem, emberek.netto
+FROM emberek
+WHERE emberek.nem = "f"
+ORDER BY emberek.netto DESC
+
+11.Feladat:
+SELECT emberek.nev, emberek.netto
+FROM emberek
+WHERE emberek.netto > 40000
+ORDER BY emberek.nev
+
+12.Feladat:
+SELECT emberek.nev, emberek.netto
+FROM emberek
+WHERE emberek.netto < 110000 AND emberek.netto > 50000 
+
+13.Feladat:
+SELECT emberek.nev, emberek.nem, emberek.gyerekszam
+FROM emberek
+WHERE emberek.nem = "f" AND emberek.gyerekszam > 1
+
+14.Feladat:
+SELECT emberek.nev, emberek.nem, emberek.beosztas, emberek.netto
+FROM emberek
+WHERE emberek.nem = "n" AND emberek.netto < 45000 AND emberek.beosztas = "ápoló"
+ORDER BY emberek.netto DESC;
+
+15.Feladat:
+SELECT emberek.nev, emberek.osztaly, emberek.gyerekszam
+FROM emberek
+WHERE emberek.osztaly = "sebészet" AND emberek.gyerekszam = 0;
+
+16.Feladat:
+SELECT emberek.nev, emberek.osztaly, emberek.gyerekszam, emberek.nem
+FROM emberek
+WHERE emberek.osztaly = "gégészet" AND emberek.nem = "n"
+ORDER BY emberek.gyerekszam DESC
+
+17.Feladat:
+SELECT emberek.nev, emberek.nem
+FROM emberek
+WHERE emberek.nem = "f" AND emberek.nev LIKE "%t%";
+
+18.Feladat:
+SELECT emberek.nev, emberek.osztaly, emberek.beosztas
+FROM emberek
+WHERE emberek.osztaly = "gégészet" AND emberek.beosztas = "orvos" OR emberek.osztaly = "sebészet"
+
+19.Feladat:
+SELECT emberek.nev, emberek.netto
+FROM emberek
+WHERE emberek.netto > 100000 OR emberek.netto < 40000
+
+20.Feladat:
+SELECT emberek.nev, emberek.osztaly, emberek.gyerekszam
+FROM emberek
+WHERE emberek.gyerekszam < 2 OR emberek.osztaly = "sebészet"
